902f94bacc057cc1012812a2b023d21f754bc43b
jokairoland <jokairoland@gmail.com>
2025-01-09 17:55:28 +0100

    Kész

--- fuggvenyhivasPalindrom.ts
+++ fuggvenyhivasPalindrom.ts
@@ -1,7 +1,7 @@
 function fuggvenyhivasPalindrom(fv:() => string):boolean{
 
     var szoveg:string = fv().trim();
-    var eredmeny:boolean = false; //false default   
+    var eredmeny:boolean = false;   
     var megforditottSzoveg:string = szoveg.split('').reverse().join('').trim();
 
     if(szoveg == megforditottSzoveg){
